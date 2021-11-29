236c33e5e4cac6e665ba6558499383a8cb0b4147
bryce messinger <brycemessinger48@gmail.com>
2021-11-28 21:48:01 -0700

    added all steps

--- about.js
+++ about.js
@@ -1,14 +1,35 @@
 console.log("hello world");
 
+const option1Input = document.getElementById('option1')
+const goalsFieldContent = document.getElementById('goalsField')
 
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Form Submitted Successfully!");
 }
 
+// Example of how not to do it on line 12
+// document.getElementById("rubberDucky").addEventListener("mouseover", alert("You are exceptionally perceptive!"))
+
+let mouseOver = document.getElementById('rubberDucky')
+mouseOver.onmouseover = () => {
+	alert("You are exceiptionally perceptive!")
+}
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+
+let option1 = document.getElementById('option1')
+option1.onclick = () => {
+	document.getElementById('goalsField').textContent = "I would like to master Java for back-end development"
+}
+
+// let btn1 = option1Input.addEventListener('change', () => {
+// 	goalsFieldContent.textContent = "I would like to master Java for back-end development"
+// }) 
---
+++ profile.js
@@ -0,0 +1,13 @@
+const btnColor = document.getElementById('color')
+const btnPlace = document.getElementById('place')
+const btnRitual = document.getElementById('ritual')
+
+let favColor = btnColor.onclick = () => {
+    alert('My Favorite Color is light blue')
+}
+let favPlace = btnPlace.onclick = () => {
+    alert('My Favorite Place is Honolulu, Hawaii')
+}
+let favRitual = btnRitual.onclick = () => {
+    alert('My Favorite Ritual is reading')
+}
